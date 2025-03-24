b663a72c699fe04327475b2e3886e6ec0210e0c5
unknown <josejoaocdias@gmail.com>
2025-03-24 05:07:58 +0000

    Basic settings created

---
+++ conf/settings.py
@@ -0,0 +1,19 @@
+"""
+Script where it's spresensts configurations aboout the app, mainly paths for the different folders.
+
+"""
+# Import important libraries
+import os
+
+# Base path to acess files
+BASE_PATH=os.getcwd()
+
+# Path to acess data folder
+DATA_PATH = os.path.join(BASE_PATH,'data')
+
+PRODUCTS_FILE = f"{DATA_PATH}/products_uuid.csv"
+SALES_FILE = f"{DATA_PATH}/sales_uuid.csv"
+STORES_FILE = f"{DATA_PATH}/stores_uuid.csv"
+
+# Output Path
+OUTPUT = f"{BASE_PATH}/output"
