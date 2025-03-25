f243569e85e083c01f27f8f594f46e55268d2952
unknown <josejoaocdias@gmail.com>
2025-03-25 23:51:18 +0000

    Test export dataframe fix 1.6 - fix path and aadd git actions for run black, flake8 and save output as artifact

--- tests.py
+++ tests.py
@@ -619,7 +619,7 @@ def test_export_dataframe_as_csv():
                 "Product A",
                 "Category A",
                 5,
-                datetime.date(2024,11,1),
+                "2025-03-01",
                 10.0
             ),
             (
@@ -629,7 +629,7 @@ def test_export_dataframe_as_csv():
                 "Product B",
                 "Category B",
                 10,
-                datetime.date(2024,11,2),
+                "2025-03-02",
                 40.0           
             ),
             (
@@ -639,7 +639,7 @@ def test_export_dataframe_as_csv():
                 "Product C",
                 "Category C",
                 30,
-                datetime.date(2024,12,1),
+                "2025-03-04",
                 160.0             
             ),
         ],
